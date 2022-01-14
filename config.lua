2de3e082a8e863e7d4523c03976f91bd44f01a9d
falcon1641 <94806141+falcon1641@users.noreply.github.com>
2022-01-14 03:28:38 -0500

    update locations

--- config.lua
+++ config.lua
@@ -5,6 +5,6 @@ Config.Gangs = {
     ['ballas'] = vector3(86.75, -1958.28, 21.12),
     ['vagos'] = vector3(0, 0, 0),
     ['cartel'] = vector3(0, 0, 0),
-    ['families'] = vector3(0, 0, 0),
-    ['triads'] = vector3(0, 0, 0)
+    ['families'] = vector3(-2588.57, 1886.32, 171.68),
+    ['triads'] = vector3(-0.57, 0.32, 0.68)
 }
